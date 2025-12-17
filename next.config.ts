0ee3961cb8023599a6f2eb15fc69696431370c24
hammaadworks <hammaadworks@gmail.com>
2025-12-17 16:28:51 +0530

    alhamdulillah deployed

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,10 @@ import type { NextConfig } from "next";
 const repo = "aacexports";
 const nextConfig: NextConfig = {
   basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   output: "export",
   images: {
     unoptimized: true,
   },
-  reactCompiler: true,
 };
 
 export default nextConfig;
